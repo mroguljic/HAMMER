a17c22cbf26b7a5cd5cbd4035ec0eca520aca04a
Matej Roguljic matej.roguljic@cern.ch <matej.roguljic@gmail.com>
2020-11-26 15:55:01 +0100

    added functions for TnP

--- TIMBER/Framework/SemileptonicFunctions.cc
+++ TIMBER/Framework/SemileptonicFunctions.cc
@@ -22,6 +22,62 @@ Float_t leptonPt(rvec_f Electron_pt,rvec_f Muon_pt,Int_t lIdx,Int_t lGeneration)
 Float_t leptonPhi(rvec_f Electron_phi,rvec_f Muon_phi,Int_t lIdx,Int_t lGeneration);
 Float_t leptonEta(rvec_f Electron_eta,rvec_f Muon_eta,Int_t lIdx,Int_t lGeneration);
 std::vector<int> goodAK4JetIdxs(Int_t nJet,rvec_f Jet_pt, rvec_f Jet_eta, rvec_f Jet_phi, rvec_i Jet_jetId, Float_t phi_l, Float_t eta_l);
+Int_t probeAK8JetIdx(Int_t nFatJet,rvec_f FatJet_pt,rvec_f FatJet_msoftdrop,rvec_f FatJet_phi,rvec_f FatJet_eta, rvec_i FatJet_jetId,Float_t lPhi,Float_t lEta);
+Int_t classifyProbeJet(Int_t fatJetIdx,rvec_f FatJet_phi,rvec_f FatJet_eta, Int_t nGenPart, rvec_f GenPart_phi,rvec_f GenPart_eta, rvec_i GenPart_pdgId, rvec_i GenPart_genPartIdxMother);
+
+Int_t classifyProbeJet(Int_t fatJetIdx,rvec_f FatJet_phi,rvec_f FatJet_eta, Int_t nGenPart, rvec_f GenPart_phi,rvec_f GenPart_eta, rvec_i GenPart_pdgId, rvec_i GenPart_genPartIdxMother){
+//1: qq, 2: bq, 3:bqq, 0 other
+    Int_t nBfromT = 0;
+    Int_t nQfromW = 0;
+    for(Int_t i=0; i<nGenPart;i++){
+        Int_t pid = GenPart_pdgId[i];
+        Int_t motherIdx = GenPart_genPartIdxMother[i];
+        Int_t motherPid = GenPart_pdgId[motherIdx];
+
+        if(motherPid==-1){//only interested in products of decays
+            continue;
+        }
+
+        Float_t dR = deltaR(GenPart_eta[i],GenPart_phi[i],FatJet_eta[fatJetIdx],FatJet_phi[fatJetIdx]);
+        Float_t dRMother = deltaR(GenPart_eta[motherIdx],GenPart_phi[motherIdx],FatJet_eta[fatJetIdx],FatJet_phi[fatJetIdx]);
+
+        if(dR>0.8 || dRMother>0.8){
+            continue;
+        }
+
+        if(TMath::Abs(pid)==5 && TMath::Abs(motherPid)==6){//if b with t as mother
+            nBfromT = nBfromT+1;
+            continue;
+        }
+        else if(TMath::Abs(pid)<6 && TMath::Abs(pid)>0 && TMath::Abs(motherPid)==24){//quark with W as mother
+            nQfromW = nQfromW+1;
+            continue;
+        }
+    }
+    if(nBfromT==1 && nQfromW ==2){
+        return 3;
+    }
+    else if(nBfromT==1 && nQfromW ==1){
+        return 2;
+    }
+    else if(nBfromT==0 && nQfromW ==2){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+Int_t probeAK8JetIdx(Int_t nFatJet,rvec_f FatJet_pt,rvec_f FatJet_msoftdrop,rvec_f FatJet_phi,rvec_f FatJet_eta, rvec_i FatJet_jetId,Float_t lPhi,Float_t lEta){
+    for(Int_t i=0; i<nFatJet;i++){
+        Int_t fatJetReq = FatJet_pt[i]>350 && FatJet_msoftdrop[i]>30 && FatJet_jetId[i]>1;
+        Float_t dR = deltaR(FatJet_eta[i],FatJet_phi[i],lEta,lPhi);//opposite hemisphere of the lepton
+        if(fatJetReq && dR>2.0){
+            return i;
+        }
+    }
+    return -1;
+}
 
 
 Float_t leptonPt(rvec_f Electron_pt,rvec_f Muon_pt,Int_t lIdx,Int_t lGeneration){
@@ -110,10 +166,6 @@ std::vector<int> goodAK4JetIdxs(Int_t nJet,rvec_f Jet_pt, rvec_f Jet_eta, rvec_f
             goodIdxs.push_back(i);
         }
     }
-/*    for(Int_t i = 0; i < goodIdxs.size(); i++){
-        std::cout<<goodIdxs[i]<<" ";
-    }
-    std::cout<<"\n";*/
     return goodIdxs;
 }
 
