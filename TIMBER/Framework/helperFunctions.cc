58a2a708b6dd9296c9a4b592ddd90764bc247147
mrogulji <matej.roguljic@cern.ch>
2020-12-09 15:01:56 +0100

    added miniIso to muon skimming criteria

--- TIMBER/Framework/helperFunctions.cc
+++ TIMBER/Framework/helperFunctions.cc
@@ -25,27 +25,29 @@ Int_t skimmingLeadingAK8Jet(Int_t nFatJet, rvec_f FatJet_eta, rvec_f FatJet_pt, 
 //Returns 1 if Leading AK8 jet(pT> 300 GeV) + eta(< 2.5) + softdrop_mass(> 30 GeV)
 Int_t skimmingTwoAK8Jets(Int_t nFatJet, rvec_f FatJet_eta, rvec_f FatJet_pt, rvec_f FatJet_msoftdrop);
 //Returns 1 if Two leading AK8 jets pt(> 300 GeV) + eta(< 2.5) + softdrop_mass(> 30 GeV)
-Int_t skimmingIsoLepton(Int_t nJet, rvec_f Jet_eta, rvec_f Jet_pt, Int_t nElectron, rvec_i Electron_cutBased,Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId);
+Int_t skimmingIsoLepton(Int_t nJet, rvec_f Jet_eta, rvec_f Jet_pt, Int_t nElectron, rvec_i Electron_cutBased,Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId,rvec_c Muon_miniIsoId);
 //Returns 1(2) if Single isolated electron(muon)(pT > 30 GeV, eta < 2.5, loose iso) + AK4 jet (pt > 30 GeV, eta < 2.5) exist
 Int_t goodElectron(Int_t nElectron, rvec_i Electron_cutBased);
 //Returns 1 if isolated loose electron exists
-Int_t goodMuon(Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId);
+Int_t goodMuon(Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId,rvec_c Muon_miniIsoId);
 //Returns 1 if isolated loose muon exists
-Int_t skimFlag(Int_t nFatJet, rvec_f FatJet_eta, rvec_f FatJet_pt, rvec_f FatJet_msoftdrop,Int_t nJet, rvec_f Jet_eta, rvec_f Jet_pt, Int_t nElectron, rvec_i Electron_cutBased,Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId);
+Int_t skimFlag(Int_t nFatJet, rvec_f FatJet_eta, rvec_f FatJet_pt, rvec_f FatJet_msoftdrop,Int_t nJet, rvec_f Jet_eta, rvec_f Jet_pt, Int_t nElectron, rvec_i Electron_cutBased,Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId,rvec_c Muon_miniIsoId);
 
-Int_t skimFlag(Int_t nFatJet, rvec_f FatJet_eta, rvec_f FatJet_pt, rvec_f FatJet_msoftdrop,Int_t nJet, rvec_f Jet_eta, rvec_f Jet_pt, Int_t nElectron, rvec_i Electron_cutBased,Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId){
+Int_t skimFlag(Int_t nFatJet, rvec_f FatJet_eta, rvec_f FatJet_pt, rvec_f FatJet_msoftdrop,Int_t nJet, rvec_f Jet_eta, rvec_f Jet_pt, Int_t nElectron, rvec_i Electron_cutBased,Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId,rvec_c Muon_miniIsoId){
     Int_t jetSkim    = skimmingLeadingAK8Jet(nFatJet,FatJet_eta,FatJet_pt,FatJet_msoftdrop);
     Int_t dijetSkim  = skimmingTwoAK8Jets(nFatJet,FatJet_eta,FatJet_pt,FatJet_msoftdrop);
-    Int_t leptonSkim = skimmingIsoLepton(nJet,Jet_eta,Jet_pt,nElectron,Electron_cutBased,nMuon,Muon_looseId,Muon_pfIsoId);//1 if ele, 2 if muon
+    Int_t leptonSkim = skimmingIsoLepton(nJet,Jet_eta,Jet_pt,nElectron,Electron_cutBased,nMuon,Muon_looseId,Muon_pfIsoId,Muon_miniIsoId);//1 if ele, 2 if muon
     //Bits: 1:jetSkim, 2:dijetSkim, 4:electronSkim,8:muonSkim
     Int_t skimScore  = jetSkim+2*dijetSkim+4*leptonSkim;
     return skimScore;
 }
 
 
-Int_t goodMuon(Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId){
+Int_t goodMuon(Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId,rvec_c Muon_miniIsoId){
     for(Int_t i=0; i<nMuon;i++){
-        if(Muon_looseId[i] && int(Muon_pfIsoId[i])>1){//1=PFIsoVeryLoose, 2=PFIsoLoose, 3=PFIsoMedium, 4=PFIsoTight, 5=PFIsoVeryTight, 6=PFIsoVeryVeryTight
+        if(Muon_looseId[i] && (int(Muon_pfIsoId[i])>1 || int(Muon_miniIsoId[i])>0)){
+        //1=PFIsoVeryLoose, 2=PFIsoLoose, 3=PFIsoMedium, 4=PFIsoTight, 5=PFIsoVeryTight, 6=PFIsoVeryVeryTight
+        //1=MiniIsoLoose, 2=MiniIsoMedium, 3=MiniIsoTight, 4=MiniIsoVeryTight)
             return 1;
         }
     }
@@ -61,12 +63,12 @@ Int_t goodElectron(Int_t nElectron, rvec_i Electron_cutBased){
 }
 
 
-Int_t skimmingIsoLepton(Int_t nJet, rvec_f Jet_eta, rvec_f Jet_pt, Int_t nElectron, rvec_i Electron_cutBased,Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId){
+Int_t skimmingIsoLepton(Int_t nJet, rvec_f Jet_eta, rvec_f Jet_pt, Int_t nElectron, rvec_i Electron_cutBased,Int_t nMuon, rvec_b Muon_looseId,rvec_c Muon_pfIsoId,rvec_c Muon_miniIsoId){
     if(nJet<1){
         return 0;
     }
     Int_t isGoodJet = TMath::Abs(Jet_eta[0])<2.5 && Jet_pt[0]>30;
-    Int_t isGoodMuon = goodMuon(nMuon,Muon_looseId,Muon_pfIsoId);
+    Int_t isGoodMuon = goodMuon(nMuon,Muon_looseId,Muon_pfIsoId,Muon_miniIsoId);
     Int_t isGoodElectron = goodElectron(nElectron,Electron_cutBased);
 
     if(isGoodElectron && isGoodMuon){
